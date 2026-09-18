2916bda28ca92bcdc726e179f7526ec593abd6ba
Karthika R <karthikakarthu1018@gmail.com>
2026-09-18 19:01:04 +0530

    Add S3 uploader test

---
+++ test_s3.py
@@ -0,0 +1,9 @@
+from s3_uploader import upload_raw_data
+
+data = {
+    "customer_id": "C001",
+    "name": "Test User",
+    "email": "test@example.com"
+}
+
+print("S3 upload function is ready.")
