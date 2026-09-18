aa282268ab9c1cad4f0f62431c199e1a4f7f220c
Karthika R <karthikakarthu1018@gmail.com>
2026-09-18 18:57:23 +0530

    Add S3 raw data uploader

---
+++ s3_uploader.py
@@ -0,0 +1,15 @@
+import boto3
+import json
+
+
+def upload_raw_data(data, bucket_name, file_name):
+    s3 = boto3.client("s3")
+
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=json.dumps(data),
+        ContentType="application/json"
+    )
+
+    print(f"Raw data uploaded to S3: {file_name}")
